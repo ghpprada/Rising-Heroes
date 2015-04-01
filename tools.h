b835153852de03f9806a6bfe6919015bf6fe6c7b
ghpprada <ghpprada@gmail.com>
2015-04-01 12:22:11 -0300

    Create tools.h

---
+++ tools.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <algorithm>
+
+using std::string;
+
+void clearScreen(){
+	system("CLS");
+}
+
+/*void cout_slowly(string s){
+	int x=0;
+	while ( s[x] != '\0')
+	{
+		cout << s[x];
+		Sleep(10);
+		x++;
+	};
+}*/
